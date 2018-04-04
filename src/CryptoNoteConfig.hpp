754e5a82f2b306147b82a4c0a890297e4a14f4c0
Dmitry <dmitry.tarasov@live.com>
2018-04-04 10:45:36 +0500

    Merge preparation

--- src/CryptoNoteConfig.hpp
+++ src/CryptoNoteConfig.hpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers.
-// Licensed under the GNU Lesser General Public License. See LICENSING.md for details.
+// Licensed under the GNU Lesser General Public License. See LICENSE for details.
 
 #pragma once
 
@@ -86,9 +86,9 @@ const size_t BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT = 10000;  // by default, blo
 const size_t BLOCKS_SYNCHRONIZING_DEFAULT_COUNT     = 100;    // by default, blocks count in blocks downloading
 const size_t COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT  = 1000;
 
-const int P2P_DEFAULT_PORT        = 18080;
-const int RPC_DEFAULT_PORT        = 18081;
-const int WALLET_RPC_DEFAULT_PORT = 18083;
+const int P2P_DEFAULT_PORT        = 28080;
+const int RPC_DEFAULT_PORT        = 28081;
+const int WALLET_RPC_DEFAULT_PORT = 28082;
 
 const size_t P2P_LOCAL_WHITE_PEERLIST_LIMIT = 1000;
 const size_t P2P_LOCAL_GRAY_PEERLIST_LIMIT  = 5000;
@@ -106,11 +106,13 @@ const uint32_t P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT      = 5000;           // 5 
 const char     P2P_STAT_TRUSTED_PUB_KEY[] = "7faf1b1a49aea1f5bc50f5df7b9087b622dc9b6e5080bae33828f71a4c7b807d";
 
 const char *const SEED_NODES[] = {
-	"80.208.225.17:18080",
-	"80.209.227.121:18080",
-	"188.166.29.175:18080",
-	"31.135.214.69:18080",
-	"178.218.207.75:18080",
+	"89.40.4.205:28080",
+	"188.166.29.175:28080",
+	//"80.208.225.17:18080",
+	//"80.209.227.121:18080",
+	//"188.166.29.175:18080",
+	//"31.135.214.69:18080",
+	//"178.218.207.75:18080",
 };
 
 struct CheckpointData {
@@ -129,8 +131,12 @@ constexpr const CheckpointData CHECKPOINTS[] = {
 	{ 80000,  "ce5ed3d782c172ef872303e01c59b99869dc1d4a9e9ac00c9d2230f26fd1df51" },
 	{ 90000,  "b8805db44aed06d03278c648648727095eaf232914f8b39fdf7f11fbb39df3fd" },
 	{ 100000, "e44dca8383873edf13ee44ca5f177a6cce6aa12e46533ce2791ecbc3b36538d1" },
-	{ 106153, "597a09b94cbfccdc532c28f668db658cb76edfcbab0151f207599488734f79c9" },
-	{ 106195, "8cdb2723b88515c57281ff73e3b30c0e924aec284528b04748a57c5f77ea0c76" },
+	//{ 106153, "597a09b94cbfccdc532c28f668db658cb76edfcbab0151f207599488734f79c9" },
+	//{ 106195, "8cdb2723b88515c57281ff73e3b30c0e924aec284528b04748a57c5f77ea0c76" },
 	{ 110000, "9041572d83e741652781036bc669756cfe1568b32053789fd91f17b2b1843049" },
+	//{ 114595, "e31ca73e4761e9c3b027469ce18ec5f52ab103db9ba1ca3838ea16e6a9cd04d0" },
+	//{ 114700, "6254290e38946fc1a00848753ae6e3bac3027b6f9fbb77d0de232c3d58b4f9ad" },
+	
+	
 };
 }  // CryptoNote
--- src/version.hpp
+++ src/version.hpp
@@ -1,16 +1,16 @@
 // Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers.
-// Licensed under the GNU Lesser General Public License. See LICENSING.md for details.
+// Licensed under the GNU Lesser General Public License. See LICENSE for details.
 
 #pragma once
 
 // defines are for Windows resource compiler
-#define BYTECOIN_VERSION_WINDOWS_COMMA 0, 18, 3, 18
-#define BYTECOIN_VERSION_WINDOWS_STRING "0.18.3.18 (BETA)"
+#define bytecoin_VERSION_WINDOWS_COMMA 0, 18, 4, 5
+#define bytecoin_VERSION_STRING "0.18.4.5 (BETA)"
 
 #ifdef __cplusplus
 
 namespace bytecoin {
-inline const char *app_version() { return BYTECOIN_VERSION_WINDOWS_STRING; }
+inline const char *app_version() { return bytecoin_VERSION_STRING; }
 }
 
 #endif
